f920f8885a2a7dc5809df34dbe8ba33d7bc7ab04
mudassarwurfel <mudassar.wurfelit@gmail.com>
2018-11-22 10:58:36 +0500

    Initial Comit

---
+++ app/src/main/java/mudasir/com/onesignaltestingproject/ApplicationClass.java
@@ -0,0 +1,22 @@
+package mudasir.com.onesignaltestingproject;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+/**
+ * Created by ${Muddassar} on 11/20/2018.
+ */
+public class ApplicationClass extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //  OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
