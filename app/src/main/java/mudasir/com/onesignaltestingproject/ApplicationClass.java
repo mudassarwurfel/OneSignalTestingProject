7c3633e807f58c2ec51b0acbba9e6ea519dce680
mudassarwurfel <mudassar.wurfelit@gmail.com>
2018-11-22 11:33:12 +0500

    Application class changes

--- app/src/main/java/mudasir/com/onesignaltestingproject/ApplicationClass.java
+++ app/src/main/java/mudasir/com/onesignaltestingproject/ApplicationClass.java
@@ -12,7 +12,7 @@ public class ApplicationClass extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
+       // OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
         // OneSignal Initialization
         OneSignal.startInit(this)
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
