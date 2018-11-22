f54af605e9d2ffa94ec45c3da72ca59f73dc9ca0
mudassarwurfel <mudassar.wurfelit@gmail.com>
2018-11-22 11:38:51 +0500

    comments added

--- app/src/main/java/mudasir/com/onesignaltestingproject/ApplicationClass.java
+++ app/src/main/java/mudasir/com/onesignaltestingproject/ApplicationClass.java
@@ -12,8 +12,9 @@ public class ApplicationClass extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
+        // OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
         // OneSignal Initialization
+        //newline from orignal
         OneSignal.startInit(this)
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
